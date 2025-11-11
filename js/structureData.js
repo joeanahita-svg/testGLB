a6b06809c0517719778cb821f8461ef017b77093
joeanahita-svg <joeanahita@gmail.com>
2025-11-11 02:28:57 +0000

    gg fddd

--- js/structureData.js
+++ js/structureData.js
@@ -99,13 +99,13 @@ const structuralData = {
 
     // Slabs (defined by 4 corner points - HORIZONTAL planes parallel to XY plane)
     // All 4 points have the SAME Z coordinate (floor level)
-    slabs: [],
+    slabs: [
+        { id: 'S1', points: [[0, 0, 4], [12, 0, 4], [12, 12, 4], [0, 12, 4]], thickness: 0.2 }      // First floor at z=4
+    ],
 
     // Walls (defined by 4 corner points - VERTICAL planes parallel to Z axis)
     // Points ordered: bottom-left, bottom-right, top-right, top-left
-    walls: [
-        { id: 'W1', points: [[0, 0, 0], [12, 0, 0], [12, 0, 8], [0, 0, 8]], thickness: 0.2 }        // Front wall along X axis at Y=0
-    ],
+    walls: [],
 
     // Supports (point location, type: 'fixed', 'pinned', 'roller')
     supports: [
